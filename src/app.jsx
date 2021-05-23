94da197a6c12e9861958704e812c867b368bbc31
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2021-05-23 21:14:03 +0200

    add jquery

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-function render() {
-    ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+function refreshHistory() {
+    ReactDOM.render(element, $("#root"));
 }
 
-render();
+// Load elements
+refreshHistory();
